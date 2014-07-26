f8b0c7392d1da159d2658daf8ad4abf2bd9b4104
somcom3x <somcom3@gmail.com>
2014-07-26 13:47:08 -0400

    peregrine bringup

--- device_peregrine.mk
+++ device_peregrine.mk
@@ -21,6 +21,13 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/motorola/msm8226-common/msm8226.mk)
-$(call inherit-product, device/motorola/msm8226-common/keylayout/keylayout.mk)
+
+# CDMA, GSM/WCDMA
+#PRODUCT_PROPERTY_OVERRIDES += \
+#	ro.telephony.default_network=5 \
+#	telephony.lteOnCdmaDevice=1 \
+#	persist.radio.mode_pref_nv10=1 \
+#	persist.radio.no_wait_for_card=1 \
+#	persist.radio.dfr_mode_set=1
