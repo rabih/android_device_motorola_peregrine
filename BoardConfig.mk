482ecb175469f502e72ea877c67986571c4a3579
somcom3x <somcom3@gmail.com>
2014-07-27 19:35:04 -0400

    Missing call

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+-USE_CAMERA_STUB := true
 #
 # This file sets variables that control the way modules are built
 # thorughout the system. It should not be used to conditionally
