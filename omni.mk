fca83e8bf4423fe625dab274e1e1363066f81888
Rabih Salamey <somcom3@carbonrom.org>
2014-12-29 18:15:52 -0600

    peregrine:omni-fy

--- omni.mk
+++ omni.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := cm_peregrine
+PRODUCT_NAME := omni_peregrine
